b4dd37baf114e53982527bee89b07313e59082e4
ts_pro <tsod_1024@163.com>
2019-09-17 15:52:47 +0800

    submit clone project

--- src/main/java/com/ibs/gitdemo/TsDemo.java
+++ src/main/java/com/ibs/gitdemo/TsDemo.java
@@ -1,11 +1,19 @@
 package com.ibs.gitdemo;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class TsDemo {
 
 	public static void main(String[] args) {
 		ReentrantLock lock = new ReentrantLock();
+		Condition cond = lock.newCondition();
+		try {
+			cond.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
