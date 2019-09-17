b70321561ea16c465933c8b57788584a6a02cd6e
ts_pro <tsod_1024@163.com>
2019-09-17 15:26:52 +0800

    my first commit in eclipse

---
+++ src/main/java/com/ibs/gitdemo/TsDemo.java
@@ -0,0 +1,11 @@
+package com.ibs.gitdemo;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TsDemo {
+
+	public static void main(String[] args) {
+		ReentrantLock lock = new ReentrantLock();
+	}
+
+}
